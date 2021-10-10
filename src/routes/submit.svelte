6ac11b1127dbb62ee8ad6019bfe8f1cca49b071b
Eric Zhang <ekzhang1@gmail.com>
2021-10-10 10:25:08 -0400

    Update placeholder text in submit page

--- src/routes/submit.svelte
+++ src/routes/submit.svelte
@@ -9,14 +9,14 @@
   <input
     required
     class="w-full border-2 border-gray-300 focus:border-yellow-500 rounded outline-none mb-2 p-2"
-    placeholder="humanities classes"
+    placeholder="intersection of three"
   />
 
   <p class="text-gray-500 mb-1">your confession:</p>
   <textarea
     required
     class="w-full h-32 border-2 border-gray-300 focus:border-yellow-500 rounded outline-none mb-2 p-2"
-    placeholder="&quot;nobody ever does the readings, but there are enough men in section that we have a discussion anyway&quot;"
+    placeholder="ever since i was a child, i've been interested in the intersection of a thing that will make me money, a less lucrative thing i'm actually interested in, and business."
   />
 
   <p class="text-gray-400 mb-2 text-sm italic">all confessions are anonymous</p>
